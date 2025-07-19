522d0cdd3f1bfa83004d4e51f11c741c9c7978ce
ouali99 <oualiob2@gmail.com>
2025-07-19 01:26:49 -0400

    add about me section

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -20,11 +20,11 @@ const Header = () => {
         </p>
         <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
             <a href="#contact" className="px-10 py-3 border rounded-white rounded-full bg-black text-white flex items-center gap-2"> 
-                contact me 
+                Contact me 
                 <Image src={assets.right_arrow} alt="" className="w-4" />
             </a>
             <a href="#sample-resume.pdf" download className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2">
-                contact me 
+                My Resume 
                 <Image src={assets.download_icon} alt="" className="w-4" />
             </a>
         </div>
